af7a11a777891000fd2270fe69d98de5064b564e
Andrey Komov <andrewkomov@gmail.com>
2022-05-17 20:25:43 +0300

    reg form commit 3

--- script.js
+++ script.js
@@ -12,28 +12,55 @@ $(document).ready(function (){
         });
     });
 })
-const Email = document.querySelector('#email');
+
+const password = document.getElementById('pass1');
+const togglePassword = document.getElementById('togglePassword');
+const repeatPassword = document.getElementById('pass2');
+const toggleRepeatPassword = document.getElementById('togglePassword2');
+
+togglePassword.addEventListener('click', function () {
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    this.classList.toggle('bi-eye');
+})
 
 
-const validateEmail = (email) => {
-    return email.match(
-        /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    );
-};
 
-const validate = () => {
-    const result = $('#result');
-    const email = $('#email').val();
-    result.text('');
 
-    if (validateEmail(email)) {
-        result.text(email + ' is valid :)');
-        result.css('color', 'green');
-    } else {
-        result.text(email + ' is not valid :(');
-        result.css('color', 'red');
-    }
-    return false;
-}
 
-$('#email').on('input', validate);
+
+
+
+
+
+
+
+
+
+
+
+
+// const Email = document.querySelector('#email');
+//
+// const validateEmail = (email) => {
+//     return email.match(
+//         /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+//     );
+// };
+//
+// const validate = () => {
+//     const result = $('#result');
+//     const email = $('#email').val();
+//     result.text('');
+//
+//     if (validateEmail(email)) {
+//         result.text(email + ' is valid :)');
+//         result.css('color', 'green');
+//     } else {
+//         result.text(email + ' is not valid :(');
+//         result.css('color', 'red');
+//     }
+//     return false;
+// }
+//
+// $('#email').on('input', validate);
