12b96092ec7a7266d25299b58a273f14fea69145
Andrey Komov <andrewkomov@gmail.com>
2022-05-19 22:09:18 +0300

    pure js version

--- scripts/script2.js
+++ scripts/script2.js
@@ -4,6 +4,11 @@ const repeatPassword = document.getElementById('pass2');
 const toggleRepeatPassword = document.getElementById('togglePassword2');
 const message = document.getElementById('message');
 const message2 = document.getElementById('message2');
+const submit = document.querySelector('#submit');
+const i = document.querySelector('#togglePassword');
+i.style.visibility = 'hidden';
+const i2 = document.querySelector('#togglePassword2');
+i2.style.visibility = 'hidden';
 
 
 const check = function() {
@@ -13,5 +18,16 @@ const check = function() {
     } if(password.value !== repeatPassword.value) {
         message.style.color = 'red';
         message.innerHTML = 'password not matching';
+        submit.disabled = true;
     }
 }
+password.addEventListener('keydown', check);
+
+const emptyPassFieldsCheck = function () {
+    if(password.value === '') {
+        message.style.color = 'red';
+        message.innerHTML = 'Please Fill Up Password Fields';
+        submit.disabled = true;
+    }
+}
+submit.addEventListener('click', emptyPassFieldsCheck);
